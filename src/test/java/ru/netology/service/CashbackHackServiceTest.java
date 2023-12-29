96d5b1ccc9a9f33a4cf652bff2066e4db5668372
Eugenia <st_shelkunova@list.ru>
2023-12-29 15:30:13 +0300

    build.gradle

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,21 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
-
-    void calculateCashBackEqually(){
+    public void calculateCashBackEqually(){
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
 
     }
     @Test
-
-    void calculateCashBackBelow(){
+    public void calculateCashBackBelow(){
         CashbackHackService service = new CashbackHackService();
         int amount = 700;
         int actual = service.remain(amount);
